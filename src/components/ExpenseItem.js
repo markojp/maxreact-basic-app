6f23b969a3d69952d37d18d1908b839b5066737d
markojp <joakim.forthewin@outlook.com>
2022-12-30 13:05:58 +0100

    Further developed ExpenseItem component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
-function ExpenseItem () {
-    return <h2>Expense Item!</h2>
+function ExpenseItem() {
+  return (
+    <div>
+      <div>March 30th 2022</div>
+      <div>
+        <h2>Rent</h2>
+        <div>400 €</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
