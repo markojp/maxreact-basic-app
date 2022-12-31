d42acb7bcb4a1500a91c69eb479260b58154b913
markojp <joakim.forthewin@outlook.com>
2022-12-31 19:02:03 +0100

    Passing data via props to ExpenseItem and generating ExpenseItems with unique key

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {id: 'e1', date: new Date(2022,0,11), title:'Insurance', amount: 100},
+    {id: 'e2',date: new Date(2022,1,12), title:'Rent', amount: 400},
+    {id: 'e3',date: new Date(2022,2,13), title:'Electricity', amount: 70},
+    {id: 'e4',date: new Date(2022,3,14), title:'Phone', amount: 50},
+  ];
+  const expenseElem = expenses.map((expense) => {
+    return <ExpenseItem key={expense.id} date={expense.date} title={expense.title} amount={expense.amount}></ExpenseItem>;
+  });
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      {expenseElem}
     </div>
   );
 }
