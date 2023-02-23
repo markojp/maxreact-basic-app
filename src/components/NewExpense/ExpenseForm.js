bc240ee7324e13dae46d7f085813ec89ac3bb076
markojp <joakim.forthewin@outlook.com>
2023-02-23 23:39:00 +0100

    Add two-way binding to ExpenseForm and clear after submit

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -62,7 +62,9 @@ const ExpenseForm = (props) => {
     event.preventDefault();
     const enteredExpense = {title: enteredTitle, amount: enteredAmount, date: new Date(enteredDate)};
     console.log(enteredExpense);
-    
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
   };
 
   return (
@@ -70,7 +72,7 @@ const ExpenseForm = (props) => {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input type="text" onChange={titleChangeHandler} value={enteredTitle}/>
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
@@ -79,6 +81,7 @@ const ExpenseForm = (props) => {
             min="0.01"
             step="0.01"
             onChange={amountChangeHandler}
+            value = {enteredAmount}
           />
         </div>
         <div className="new-expense__control">
@@ -88,6 +91,7 @@ const ExpenseForm = (props) => {
             min="2019-01-01"
             max="2023-12-31"
             onChange={dateChangeHandler}
+            value={enteredDate}
           />
         </div>
       </div>
