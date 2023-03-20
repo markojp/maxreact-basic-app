8d586740b7de0fbb78010934f8a36ef8ac8acc69
markojp <joakim.forthewin@outlook.com>
2023-03-20 19:22:25 +0100

    Stateful list able to add new expenses. Fixed key props on selector year

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
+import { useState } from "react"; 
+
+const DUMMY_EXPENSES = [
+  { id: "e1", date: new Date(2022, 0, 11), title: "Insurance", amount: 100 },
+  { id: "e2", date: new Date(2021, 1, 12), title: "Rent", amount: 400 },
+  { id: "e3", date: new Date(2020, 2, 13), title: "Electricity", amount: 70 },
+  { id: "e4", date: new Date(2019, 3, 14), title: "Phone", amount: 50 },
+  { id: "e5", date: new Date(2019, 1, 12), title: "Clothes", amount: 340 },
+];
 
 const App = () => {
-  const expenses = [
-    { id: "e1", date: new Date(2022, 0, 11), title: "Insurance", amount: 100 },
-    { id: "e2", date: new Date(2021, 1, 12), title: "Rent", amount: 400 },
-    { id: "e3", date: new Date(2020, 2, 13), title: "Electricity", amount: 70 },
-    { id: "e4", date: new Date(2019, 3, 14), title: "Phone", amount: 50 },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
   const years = [2019, 2020, 2021, 2022, 2023];
   const selectedYear = 2019;
 
   const addExpenseHandler = (expense) => {
     console.log('In App.js');
-    console.log(expense);
+    setExpenses(prevExpenses => {
+      return [expense, ...prevExpenses];
+    })
   }
 
   return (
--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -2,7 +2,7 @@ import './ExpensesFilter.css';
 
 const ExpensesFilter = (props) => {
     const optionElements = props.options.map(year=> {
-        return <option value={year}>{year}</option>;
+        return <option key={year} value={year}>{year}</option>;
     });
 
     const dropDownChangeHandler = (event) => {
