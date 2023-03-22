29495b87721f3af1688aeb8c5c107cd2e219ce34
markojp <joakim.forthewin@outlook.com>
2023-03-22 18:56:09 +0100

    Add conditional element to NewExpense component. Show button when not editing and form if editing.

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,15 +1,32 @@
 import './NewExpense.css';
 import ExpenseForm from './ExpenseForm';
+import { useState } from 'react';
 
 const NewExpense = (props) => {
+    const [isEditing, setIsEditing] = useState(false);
+    
     const saveExpenseDataHandler = (expenseData) => {
         const expense = {...expenseData, id: Math.random().toString()};
         props.onAddExpense(expense);
     };
 
+    const onStopEditingHandler = () => {
+        setIsEditing(false);
+    }
+
+    const onStartEditingHandler = () => {
+        setIsEditing(true);
+    }
+
+    let conditionalElem = <button type='button' onClick={onStartEditingHandler}>Add Expense</button>
+
+    if(isEditing){
+        conditionalElem = <ExpenseForm onCancel={onStopEditingHandler} onSaveExpenseData={saveExpenseDataHandler} />
+    }
+
     return (
         <div className='new-expense'>
-            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+            {conditionalElem}
         </div>
     );
 }
